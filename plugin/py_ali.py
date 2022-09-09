6ba5d0474855deeb75a28e9f4b3ddda51f27fa4e
c7877 <111748626+c7877@users.noreply.github.com>
2022-09-09 21:03:26 +0800

    Update py_ali.py

--- plugin/py_ali.py
+++ plugin/py_ali.py
@@ -391,7 +391,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 		url = 'https://api.aliyundrive.com/token/refresh'
 		if len(self.authorization) == 0 or self.timeoutTick - self.localTime <= 600:
 			form = {
-				'refresh_token':'4acb3ad2f2254ba1b566279f7cd98ba3'
+				'refresh_token':'3781469b13074e4cac1e0e7113e4fa20'
 			}
 			rsp = requests.post(url,json = form,headers=self.header)
 			jo = json.loads(rsp.text)
